21c5919a98aec9bda57af99d6f0a9aff0edec784
Matthias Bathke <info@straightvisions.com>
2019-07-24 20:44:58 +0200

    ADD LOOP SUPPORT

--- lib/frontend/tpl/default.php
+++ lib/frontend/tpl/default.php
@@ -1,10 +1,57 @@
-<div class="<?php echo $this->get_prefix(); ?>">
-	<span class="<?php echo $this->get_prefix( 'title' ); ?>">
-		<?php _e( 'Tags', 'sv100' ); ?>
-	</span>
-	<div class="<?php echo $this->get_prefix( 'wrapper' ); ?>">
-	<?php
-	the_tags( '', '', '' );
+<?php
+if(in_the_loop()){
 	?>
+	<div class="<?php echo $this->get_prefix(); ?>">
+		<span class="<?php echo $this->get_prefix( 'title' ); ?>">
+			<?php _e( 'Tags', 'sv100' ); ?>
+		</span>
+		<div class="<?php echo $this->get_prefix( 'wrapper' ); ?>">
+			<?php
+			the_tags( '', '', '' );
+			?>
+		</div>
 	</div>
-</div>
+	<?php
+}else {
+	$tags = get_tags(array('pad_counts' => true));
+
+	if (count($tags) > 0) {
+		$counts = $tag_links = array();
+
+		foreach ($tags as $t) {
+			$counts[$t->name] = $t->count;
+			$tag_links[$t->name] = get_tag_link($t->term_id);
+		}
+
+		asort($counts);
+
+		$counts = array_reverse($counts, true);
+		$i = 0;
+		?>
+		<div class="<?php echo $this->get_prefix(); ?>">
+		<span class="<?php echo $this->get_prefix('title'); ?>">
+			<?php _e('Tags', 'sv100'); ?>
+		</span>
+			<div class="<?php echo $this->get_prefix('wrapper'); ?>">
+				<?php
+				foreach ($counts as $t => $count) {
+					$i++;
+					$tag_link = esc_url($tag_links[$t]);
+					$t = str_replace(' ', '&nbsp;', esc_html($t));
+
+					if ($i <= ($settings['limit'] ? $settings['limit'] : 3)) {
+						?>
+						<a href="<?php echo $tag_link; ?>"
+						   title="<?php echo esc_attr(sprintf(__('View all posts in %s', 'sv100'), $t)); ?>">
+							<?php echo $t; ?>
+						</a>
+						<?php
+					}
+				}
+				?>
+			</div>
+		</div>
+		<?php
+	}
+}
+?>
